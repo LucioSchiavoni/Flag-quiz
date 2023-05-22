d93ba1fb6e54b6c81e9b1ca25c4346185e50bd31
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-05-22 19:28:17 -0300

    mejorar respuestas

--- client/pages/ShowQuiz.jsx
+++ client/pages/ShowQuiz.jsx
@@ -18,7 +18,7 @@ const ShowQuiz = () => {
   const getItems = async () => {
     try {
     
-      const res = await axios.get("https://restcountries.com/v3.1/all");
+      const res = await axios.get("/api/all");
       const allFlags = res.data;
 
       // Obtener todas las banderas y mezclarlas
